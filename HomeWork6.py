0dabbbb07b7e0089cf8bc3da4d4f98f38abfc7d2
Rashid20071601 <153351715+Rashid20071601@users.noreply.github.com>
2024-09-29 09:21:41 +0300

    Update HomeWork6.py

--- HomeWork6.py
+++ HomeWork6.py
@@ -5,8 +5,11 @@ def get_matrix(n, m, value):
         for j in range(m):
             list_.append(value)
         matrix.append(list_)
-    print(matrix)
+    return matrix
 
-get_matrix(2, 2, 10)
-get_matrix(3, 5, 42)
-get_matrix(4, 2, 13)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
